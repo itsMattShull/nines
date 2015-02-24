405f3e615e60b5a2c11b6f8fadb80ae13afa3c72
Matt Shull <derekshull@gmail.com>
2015-02-23 22:42:45 -0600

    Update getInfo.php

--- reporting/getInfo.php
+++ reporting/getInfo.php
@@ -2,14 +2,14 @@
 //connect to the database so we can check, edit, or insert data to our users table
 $conn = new PDO("mysql:host={{HOSTNAME}};dbname={{DATABASE-NAME}}", {{USERNAME}}, "{{PASSWORD}}");  
 $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-$sql= $conn->prepare('SELECT DISTINCT url FROM perf');
+$sql= $conn->prepare('SELECT DISTINCT url FROM webpagetest_results');
 $sql->execute();
 $urlarray = array();
 while ($row=$sql->fetch()) {
     $url=$row['url'];
     $urlarray[] = array($url);
 }
-$sql= $conn->prepare('SELECT DISTINCT country FROM perf');
+$sql= $conn->prepare('SELECT DISTINCT country FROM webpagetest_results');
 $sql->execute();
 $countryarray = array();
 while ($row=$sql->fetch()) {
